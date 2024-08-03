3a6e47942463a5fe3a7800901bc88389e4877a00
SegunNano <segunfadipe97@gmail.com>
2024-08-03 05:09:29 +0100

    UpdateProfile completed

--- frontend/src/pages/User/Profile.jsx
+++ frontend/src/pages/User/Profile.jsx
@@ -15,7 +15,7 @@ const Profile = () => {
 
     const { userInfo } = useSelector(state => state.auth);
 
-    const [updateProfile, { isLoading: loadingUpdateprofile }] = useProfileMutation();
+    const [updateProfile, { isLoading: loadingUpdateProfile }] = useProfileMutation();
 
     useEffect(() => {
         setUsername(userInfo.username);
@@ -24,9 +24,55 @@ const Profile = () => {
 
     const dispatch = useDispatch();
 
-    return (
-        <div>
+    const handleSubmit = async (e) => {
+        e.preventDefault();
 
+        if (password !== confirmPassword) {
+            toast.error('Passwords do not match');
+        } else {
+            try {
+                const res = await updateProfile({ _id: userInfo._id, username, email, password }).unwrap();
+                dispatch(setCredentials({ ...res }));
+                toast.success('Profile Updated Succefully');
+            } catch (err) {
+                console.log(err);
+                toast.error(err?.data?.message || err.data.message);
+            }
+        }
+    };
+
+
+    return (
+        <div className="container mx-auto p-4 mt-[10rem]">
+            <div className="flex justify-center align-center md:flex md:space-x-4">
+                <div className="md:w-1/3">
+                    <h2 className="text-2xl font-semibold mb-4">Update Profile</h2>
+                    <form onSubmit={handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block mb-2">Name</label>
+                            <input type="text" name="" id="" placeholder="Enter Name" className="form-input p-4 rounded-sm w-full" value={username} onChange={(e) => setUsername(e.target.value)} />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-2">Email</label>
+                            <input type="email" name="" id="" placeholder="Enter Email" className="form-input p-4 rounded-sm w-full" value={email} onChange={(e) => setEmail(e.target.value)} />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-2">Password</label>
+                            <input type="password" name="" id="" placeholder="Enter Password" className="form-input p-4 rounded-sm w-full" value={password} onChange={(e) => setPassword(e.target.value)} />
+                        </div>
+                        <div className="mb-4">
+                            <label className="block mb-2">Confirm Password</label>
+                            <input type="password" placeholder="Confirm Password" name="" id="" className="form-input p-4 rounded-sm w-full" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} />
+                            <div className="flex justify-between">
+                                <button type="submit" className="bg-pink-500 text-white py-2 px-4 rounded hover:bg-pink-600">Update</button>
+
+                                <Link to='/user-orders' className="bg-pink-600 text-white py-2 px-4 rounded hover:bg-pink-700">My Orders</Link>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+                {loadingUpdateProfile && <Loader />}
+            </div>
         </div>
     );
 };
