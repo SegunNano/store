7c0cbba55a1558722ecf5691f292a2350026aad7
SegunNano <segunfadipe97@gmail.com>
2024-08-12 05:50:50 +0100

    ProductList.jsx

---
+++ frontend/src/pages/Admin/ProductList.jsx
@@ -0,0 +1,65 @@
+import { useState } from "react";
+import { useNavigate } from "react-router";
+import { useCreateProductMutation, useUploadProductImageMutation } from "../../redux/api/productApiSlice";
+import { useFetchCategoriesQuery } from "../../redux/api/categoryApiSlice";
+import { toast } from "react-toastify";
+
+
+
+const ProductList = () => {
+    const [image, setImage] = useState('');
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [price, setPrice] = useState('');
+    const [category, setCategory] = useState('');
+    const [quantity, setQuantity] = useState('');
+    const [brand, setBrand] = useState('');
+    const [stock, setStock] = useState('');
+    const [imageUrl, setImageUrl] = useState(null);
+
+    const navigate = useNavigate();
+
+    const [uploadProductImage] = useUploadProductImageMutation();
+    const [createProduct] = useCreateProductMutation();
+    const { data: categories } = useFetchCategoriesQuery();
+
+    return (
+        <div className="cantainer xl:mx-[9rem] sm:mx-[0]">
+            <div className="flex flex-col md:flex-row">
+                {/* <AdminMenu/> */}
+                <div className="md:w-3/4 p-3">
+                    <div className="h-12">Create Product</div>
+
+                    {imageUrl && (
+                        <div className="text-center">
+                            <img src={imageUrl} alt="product" className="block mx-auto max-h-200px" />
+                        </div>
+                    )}
+
+                    <div className="mb-3">
+                        <label htmlFor="" className="border  px-4 block w-full text-center rounded-lg cursor-pointer font-bold py-11">
+                            {image ? image.name : "Upload Image"}
+                            <input type="file" name="image" accept="image/*" onChange={uploadImageHandler} id="" className={!image && 'hidden'} />
+                        </label>
+                    </div>
+
+                    <div className="p-3">
+                        <div className="flex flex-wrap">
+                            <div className="one">
+                                <label htmlFor="name">Name</label><br />
+                                <input type="text" name="" id="" className="p-4 mb-3 border rounded-lg bg-[#101011] w-[30rem]" value={name} onChange={e => setName(e.target.value)} />
+                            </div>
+                            <div className="two">
+                                <label htmlFor="price">Name</label><br />
+                                <input type="number" name="" id="" className="p-4 mb-3 border rounded-lg bg-[#101011] w-[30rem]" value={price} onChange={e => setPrice(e.target.value)} />
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default ProductList;
---
+++ frontend/src/redux/api/productApiSlice.js
@@ -0,0 +1,100 @@
+import { PRODUCT_URL, UPLOAD_URL } from "../constants";
+import { apiSlice } from "./apiSlice";
+
+export const productApiSlice = apiSlice.injectEndpoints({
+    endpoints: (builder) => ({
+        getProducts: builder.query({
+            query: ({ keyword }) => ({
+                url: `${PRODUCT_URL}`,
+                params: { keyword }
+            }),
+
+            keepUnusedDataFor: 5,
+            providesTags: ['Product']
+        }),
+
+        fetchProduct: builder.query({
+            query: (productId) => `${PRODUCT_URL}/${productId}`,
+            providesTags: (result, error, productId) => [
+                { type: "Product", id: productId }
+            ]
+        }),
+
+
+        fetchAllProducts: builder.query({
+            query: () => `${PRODUCT_URL}/allproducts`
+        }),
+
+
+        readProduct: builder.query({
+            query: productId => ({
+                url: `${PRODUCT_URL}/${productId}`
+            }),
+            keepUnusedDataFor: 5
+        }),
+
+        createProduct: builder.mutation({
+            query: productData => ({
+                url: `${PRODUCT_URL}`,
+                method: `POST`,
+                body: productData
+            }),
+            invalidatesTags: ['Product']
+        }),
+
+        uploadProductImage: builder.mutation({
+            query: data => ({
+                url: `${UPLOAD_URL}`,
+                method: 'POST',
+                body: data
+            })
+        }),
+
+
+        updateProduct: builder.mutation({
+            query: ({ productId, formData }) => ({
+                url: `${PRODUCT_URL}/${productId}`,
+                method: 'PUT',
+                body: formData
+            }),
+        }),
+
+        deleteProduct: builder.mutation({
+            query: productId => ({
+                url: `${PRODUCT_URL}/${productId}`,
+                method: 'DELETE',
+            }),
+            providesTags: ['Product'],
+        }),
+
+        createReview: builder.mutation({
+            query: data => ({
+                url: `${PRODUCT_URL}/${data.productId}`,
+                method: 'POST',
+                body: data
+            })
+        }),
+
+        fetchTopProducts: builder.query({
+            query: () => {
+                url: `${PRODUCT_URL}/top`;
+            },
+            keepUnusedDataFor: 5,
+
+        }),
+
+        fetchNewProducts: builder.query({
+            query: () => {
+                url: `${PRODUCT_URL}/new`;
+            },
+            keepUnusedDataFor: 5,
+
+        }),
+
+
+    })
+});
+
+
+
+export const { useGetProductsQuery, useFetchProductQuery, useFetchAllProductsQuery, useReadProductQuery, useUploadProductImageMutation, useCreateProductMutation, useUpdateProductMutation, useDeleteProductMutation, useCreateReviewMutation, useFetchTopProductsQuery, useFetchNewProductsQuery } = productApiSlice;
